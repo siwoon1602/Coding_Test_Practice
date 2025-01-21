65ff5dc646efb96c54e192e7118d39d8ce159f17
Yoon Siwoon (윤시운) <139972064+siwoon1602@users.noreply.github.com>
2025-01-21 16:59:20 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -0,0 +1,10 @@
+function solution(str1, str2) {
+    let answer = "0"
+    let result = str1.includes(str2)
+    if (result){
+    return answer = 1;
+    } else {
+       return answer = 2;
+    }
+    
+}
