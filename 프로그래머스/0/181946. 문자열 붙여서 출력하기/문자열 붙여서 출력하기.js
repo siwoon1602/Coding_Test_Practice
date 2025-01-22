1e17cefebc391d3007077cb9aca3ed91b33912d9
Yoon Siwoon (윤시운) <139972064+siwoon1602@users.noreply.github.com>
2025-01-22 22:15:29 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 58.74 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str1 = input[0].toString();
+    str2 = input[1].toString();
+    
+    console.log(str1 + str2)
+    
+});
